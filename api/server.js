632a5a65dad2e27f86534f086c7a2a3f58f16e0d
Lukecp <lukecp@pm.me>
2021-03-14 23:33:41 -0400

    Complted MVP

--- api/server.js
+++ api/server.js
@@ -1,2 +1,11 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const PostsRouter = require('./posts/posts-router');
+
+const app = express();
+module.exports = app;
+
+app.use(express.json());
+
+app.use('/api/posts', PostsRouter);
